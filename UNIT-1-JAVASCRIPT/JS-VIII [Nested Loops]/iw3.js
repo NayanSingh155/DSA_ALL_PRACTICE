7d5b14f7405191b96700276913df57a9974bf8cc
NayanSingh155 <105914443+NayanSingh155@users.noreply.github.com>
2022-09-30 15:17:18 +0530

    JS-VIII [Nested Loops]iw

---
+++ UNIT-1-JAVASCRIPT/JS-VIII [Nested Loops]/iw3.js
@@ -0,0 +1,14 @@
+// Problem 3: Print a box patterns using *
+
+
+var bag="";
+  for(var son=1;son<=10;son++)
+    {
+      bag=bag+"*";
+      
+    }
+console.log(bag);
+for(var father=1;father=2;father++)
+  {
+    bag=bag+"*        ";
+  }
